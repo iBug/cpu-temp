56a7897b09918c87b8a4aa1069b96e28c4a27531
iBug <7273074+iBug@users.noreply.github.com>
2019-03-15 20:37:54 +0800

    Add monitoring for CPU frequency

--- main.py
+++ main.py
@@ -6,20 +6,26 @@ import curses
 
 
 class Config:
-    source = "/sys/class/thermal/thermal_zone0/temp"
+    temp_source = "/sys/class/thermal/thermal_zone0/temp"
+    freq_source = "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
 
-    prefix = "CPU Temperature: "
+    temp_prefix = "CPU Temperature: "
+    freq_prefix = "CPU Frequency: "
     color = False
 
 
 def get_cpu_temp():
-    with open(Config.source, "r") as f:
+    with open(Config.temp_source, "r") as f:
         return int(f.read()) / 1000.0
 
 
-def print_cpu_temp():
+def get_cpu_freq():
+    with open(Config.freq_source, "r") as f:
+        return int(f.read()) / 1000.0  # This converts KHz to MHz
+
+
+def format_cpu_temp():
     temp = get_cpu_temp()
-    end = "\x1B[0m"
     if Config.color:
         if temp >= 80:
             color = "31;1"
@@ -29,14 +35,46 @@ def print_cpu_temp():
             color = "36;1"
         else:
             color = "32;1"
-        print("\r\x1B[K{}\x1B[{}m{}'C".format(Config.prefix, color, temp), end=end)
+        return "\x1B[K{}\x1B[{}m{}'C".format(Config.temp_prefix, color, temp)
     else:
-        print("\r\x1B[K{}{}'C".format(Config.prefix, temp), end=end)
+        return "\x1B[K{}{}'C".format(Config.temp_prefix, temp)
+
+
+def format_cpu_freq():
+    freq = get_cpu_freq()
+    if freq >= 1000:
+        freq_s = "{:.2f} GHz".format(freq / 1000)
+    else:
+        freq_s = "{:g} MHz".format(freq)
+    if Config.color:
+        if freq >= 1200:
+            color = "31;1"
+        elif freq >= 1000:
+            color = "33;1"
+        elif freq >= 800:
+            color = "36;1"
+        else:
+            color = "32;1"
+        return "\x1B[K{}\x1B[{}m{}".format(Config.freq_prefix, color, freq_s)
+    else:
+        return "\x1B[K{}{}".format(Config.freq_prefix, freq_s)
+
+
+def print_cpu_temp():
+    print("\r" + format_cpu_temp(), end="\x1B[0m")
+
+
+def print_cpu_freq():
+    print("\r" + format_cpu_freq(), end="\x1B[0m")
+
+
+def print_all():
+    print("\x1B[F" + format_cpu_temp() + "\x1B[0m\n" + format_cpu_freq(), end="\x1B[0m")
 
 
 def main_loop():
     while True:
-        print_cpu_temp()
+        print_all()
         time.sleep(1)
 
 
